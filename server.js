4f5fc63a898d93cf615d8c57400e7b457cdd9cca
Jakub Mazurek <jmazurek@sala514i.zsl>
2023-02-24 08:56:52 +0100

    Adapt webpack usage, remember to avoid import node core modules to bundled .js files

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-import express from 'express'
-import path from 'path'
+const express = require('express')
+const path = require('path')
 
+const port = 3000
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'src')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req,res) => {
-    res.send("Data from the server!")
+    res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
-app.listen(3000, () => {
-    console.log("Server is up!");
+app.listen(port, () => {
+    console.log(`Server is up on localhost:${port}`);
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 module.exports = {
-  entry: {
-    server: './server.js',
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-    filename: '[name].js'
-  },
-  target: 'node',
-  node: {
-    // Need this when working with express, otherwise the build fails
-    __dirname: false,   // if you don't put this is, __dirname
-    __filename: false,  // and __filename return blank or /
-  },
-  externals: [nodeExternals()], // Need this to avoid error when working with Express
-  module: {
-    rules: [
-      {
-        // Transpiles ES6-8 into ES5
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        // Loads the javacript into html template provided.
-        // Entry point is set below in HtmlWebPackPlugin in Plugins 
-        test: /\.html$/,
-        use: [{loader: "html-loader"}]
-      }
+    mode: 'development',
+    entry: {
+        bundle: path.resolve(__dirname, 'src/index.js')
+    },
+    output : {
+        path: path.resolve(__dirname,'dist'),
+        filename: '[name].js',
+        clean: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Webpack App',
+            filename: 'index.html',
+            template: 'src/index.html'
+        })
     ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./index.html",
-      filename: "./index.html",
-      excludeChunks: [ 'server' ]
-    })
-  ]
 }
