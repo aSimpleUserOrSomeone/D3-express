eacc9a700c900a4a74fe65e952ef2c32477470e4
hardToPickANameIsntIt <37453773+aSimpleUserOrSomeone@users.noreply.github.com>
2023-02-24 11:52:39 +0000

    Attempt to add handlebars to the project, but fail for now

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const path = require('path')
+const handlebars = require('express-handlebars')
 
 const port = 3000
 const app = express()
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, './views'));
+
+
 app.use(express.static(path.join(__dirname, 'dist')))
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist/index.html'))
 })
 
